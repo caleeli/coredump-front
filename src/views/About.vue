7ef66e894a72d818582efb8e16b7d30e51853872
David Callizaya <davidcallizaya@gmail.com>
2021-05-01 12:00:02 -0400

    Try navbar

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about h-100">
-    <h1>This is an about page</h1>
+  <div>
+    <div>
+      <b-navbar toggleable="md" type="dark" variant="info">
+        <b-navbar-brand>Cartera</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+
+            <b-nav-item-dropdown text="Accounts" right>
+              <b-dropdown-item href="#">Edit Account Titles</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown text="Transactions" right>
+              <b-dropdown-item href="#">1. --------------------</b-dropdown-item>
+              <b-dropdown-item href="#">2. --------------------</b-dropdown-item>
+              <b-dropdown-item href="#">3. --------------------</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown text="Reports" right>
+              <b-dropdown-item href="#">1. Net Worth Report</b-dropdown-item>
+              <b-dropdown-item href="#">2. --------------------</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
 </template>
